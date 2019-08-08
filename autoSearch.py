7e9e95f8d8f402f22e32b9deae60ef5997520c99
JohnnyHsieh1020 <46452717+JohnnyHsieh1020@users.noreply.github.com>
2019-08-08 22:10:08 +0800

    Web Crawler-practice(Selenium)

--- autoSearch.py
+++ autoSearch.py
@@ -1,44 +1,21 @@
 #----import----#
 from selenium import webdriver
 import time
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
 #--------------#
 url = "https://www.google.com/"
-path = 'chromedriver'
-
+driver = webdriver.Chrome() 
+driver.get(url)
 #----自動化搜尋----#
 def get_result(search_name):
-    driver = webdriver.Chrome(path) 
-    driver.get(url)
-# 搜尋欄位
-    search = driver.find_element_by_xpath("//input[@class='gLFyf gsfi']")
-    #print("column check")
-# 輸入搜尋關鍵字
-    search.send_keys(search_name)
-    #print("type check")
-# 搜尋按鈕
-    submit = driver.find_element_by_xpath("//div[@class='aajZCb']/div[@class='VlcLAe']/center/input[@class='gNO89b']")
-    #print("search button check")
-# 點擊搜尋按鈕
+    driver.find_element_by_name("q").send_keys(search_name)
+    submit = driver.find_element_by_name("btnK")
     time.sleep(3)
     submit.click()
-    #print("click check 1")
-# 選擇預設好的網頁
     link = driver.find_element_by_xpath("//a/h3[@class='LC20lb']")
-    #print("link check")
-# 點擊連結至網頁
     time.sleep(2)
     link.click()
-    #print("click check 2")
-# 擷取當下的網址
-    current_url = driver.current_url
-    print(current_url)
-
-    time.sleep(3)
-    driver.close()
-    #print("close check")
-    return "search success"
+    #----搜尋結束----#
+    time.sleep(5)
+    driver.quit()
 #--------------#
-result = get_result("Ntub")
-print(result)
+get_result("Ntub")
